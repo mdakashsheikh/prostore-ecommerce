6c2d457c85599f52ce8fd6b3e3115faac0f41ece
MdAkashSheikh <nmakashsheikh@gmail.com>
2025-10-15 15:24:04 +0600

    added dark and light mood

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
-import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants"
 import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants"
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({subsets: ['latin']});
 
@@ -21,11 +21,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
